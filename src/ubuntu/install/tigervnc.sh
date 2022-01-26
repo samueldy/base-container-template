1c9dad5129ba48ae3899ef3c2a8094b03c25e917
Samuel Young <samueldy@umich.edu>
2022-01-26 11:51:10 -0500

    Addressed CVE-2021-4034 and adapted for TigerVNC 1.12

--- src/ubuntu/install/tigervnc.sh
+++ src/ubuntu/install/tigervnc.sh
@@ -2,4 +2,11 @@
 set -e
 
 echo "Install TigerVNC server"
-wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -qO- https://sourceforge.net/projects/tigervnc/files/stable/1.12.0/tigervnc-1.12.0.x86_64.tar.gz | tar xz --strip 1 -C /
+
+# Now restore old behavior by putting the vncserver Perl script back onto $PATH.
+# Thanks to https://www.linuxfromscratch.org/blfs/view/svn/xsoft/tigervnc.html
+install -m755 --owner=root /usr/libexec/vncserver /usr/bin
+
+# Try to get XFCE to start under VNC
+sed -i -e 's|Exec=startxfce4|Exec=dbus-launch startxfce4|g' /usr/share/xsessions/xfce.desktop
